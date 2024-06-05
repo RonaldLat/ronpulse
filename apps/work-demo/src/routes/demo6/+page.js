d310a4a54899bc9bd87289b15c6c8a041ec8891a
Ronald Otieno <ronnlats@gmail.com>
2024-06-05 01:43:02 +0000

    images merge

--- apps/work-demo/src/routes/demo6/+page.js
+++ apps/work-demo/src/routes/demo6/+page.js
@@ -7,38 +7,37 @@ export function load({ params }) {
 				name: 'Chocolate Berry Cake',
 				description: 'A delightful chocolate cake topped with fresh berries and a hint of mint.',
 				price: 2500,
-				image: 'path_to_your_image.jpg'
+				image: 'blackforest1-transp.png'
 			},
 			{
 				name: 'Vanilla Dream Cake',
 				description:
 					'A classic vanilla cake with layers of creamy frosting and fresh strawberries.',
 				price: 2200,
-				image: 'path_to_your_image.jpg'
+				image: 'blackforest1-transp.png'
 			},
 			{
 				name: 'Red Velvet Cake',
 				description:
 					'A rich red velvet cake with a smooth cream cheese frosting and a touch of elegance.',
 				price: 2800,
-				image: 'path_to_your_image.jpg'
+				image: 'blackforest1-transp.png'
 			},
 			{
 				name: 'Lemon Poppy Seed Cake',
 				description: 'A zesty lemon cake with crunchy poppy seeds and a tangy lemon glaze.',
 				price: 2000,
-				image: 'path_to_your_image.jpg'
+				image: 'blackforest1-transp.png'
 			},
 			{
 				name: 'Carrot Walnut Cake',
 				description: 'A moist carrot cake with crunchy walnuts and a creamy cream cheese frosting.',
 				price: 2400,
-				image: 'path_to_your_image.jpg'
+				image: 'blackforest1-transp.png'
 			},
 			{
 				name: 'Black Forest Cake',
-				description:
-					'A classic black forest cake with layers of chocolate, cherries, and whipped cream.',
+				description: 'blackforest1-transp.png',
 				price: 2600,
 				image: 'path_to_your_image.jpg'
 			}

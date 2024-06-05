fd45a4f83e8cf6a59e46148350c695b8e73c6226
Ronald Otieno <ronnlats@gmail.com>
2024-06-05 09:05:24 +0000

    gitpod changes

--- apps/work-demo/src/routes/demo6/+page.js
+++ apps/work-demo/src/routes/demo6/+page.js
@@ -8,7 +8,7 @@ export function load({ params }) {
 				description: 'A delightful chocolate cake topped with fresh berries and a hint of mint.',
 				price: 2500,
 				// image: 'blackforest1-transp.png'
-				image: 'output.png'
+				image: 'blackforest1.1.png'
 			},
 			{
 				name: 'Vanilla Dream Cake',
@@ -41,6 +41,56 @@ export function load({ params }) {
 				description: 'blackforest1-transp.png',
 				price: 2600,
 				image: 'path_to_your_image.jpg'
+			}
+		],
+
+		cakes_slides: [
+			{
+				name: 'Chocolate Berry Cake',
+				price: 2500,
+				numbersSold: 150,
+				ratings: 4.8,
+				description: 'A delightful chocolate cake topped with fresh berries and a hint of mint.',
+				availableSizes: ['Small', 'Medium', 'Large'],
+				image: 'blackforest2.png'
+			},
+			{
+				name: 'Vanilla Dream Cake',
+				price: 2200,
+				numbersSold: 200,
+				ratings: 4.7,
+				description:
+					'A classic vanilla cake with layers of creamy frosting and fresh strawberries.',
+				availableSizes: ['Small', 'Medium', 'Large'],
+				image: 'images/carrotcake1.png'
+			},
+			{
+				name: 'Red Velvet Cake',
+				price: 2800,
+				numbersSold: 180,
+				ratings: 4.9,
+				description:
+					'A rich red velvet cake with a smooth cream cheese frosting and a touch of elegance.',
+				availableSizes: ['Small', 'Medium', 'Large'],
+				image: 'images/carrotcake2.png'
+			},
+			{
+				name: 'Lemon Poppy Seed Cake',
+				price: 2000,
+				numbersSold: 130,
+				ratings: 4.6,
+				description: 'A zesty lemon cake with crunchy poppy seeds and a tangy lemon glaze.',
+				availableSizes: ['Small', 'Medium'],
+				image: 'chocolatecake2.png'
+			},
+			{
+				name: 'Carrot Walnut Cake',
+				price: 2400,
+				numbersSold: 170,
+				ratings: 4.7,
+				description: 'A moist carrot cake with crunchy walnuts and a creamy cream cheese frosting.',
+				availableSizes: ['Small', 'Medium', 'Large'],
+				image: 'chocolatecake3.png'
 			}
 		]
 	};

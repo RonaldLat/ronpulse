ab2b4520a1cbb4f7a5926af489807b0973d95037
Ronald Otieno <ronnlats@gmail.com>
2024-06-25 11:23:53 +0300

    crm not running

--- apps/work-demo/src/routes/admin/+page.server.js
+++ apps/work-demo/src/routes/admin/+page.server.js
@@ -2,6 +2,16 @@ export async function load({ params }) {
 	const { id } = params;
 
 	return {
-		id
+		id,
+		demos
 	};
 }
+const demos = [
+	{ name: 'demo1', url: 'demo1', niche: 'generic' },
+	{ name: 'demo2', url: 'demo2', niche: 'dashboard starter' },
+	{ name: 'demo3', url: 'demo3', niche: 'port/construction' },
+	{ name: 'demo4', url: 'demo4', niche: 'gym' },
+	{ name: 'demo5', url: 'demo5', niche: 'generic' },
+	{ name: 'demo6', url: 'demo6', niche: 'bakery' },
+	{ name: 'demo7', url: 'demo7', niche: 'bakery' }
+];
--- apps/work-demo/src/routes/admin/+page.svelte
+++ apps/work-demo/src/routes/admin/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { BrowserMockup, VerticalTimeline, GradientHeading } from '@repo/ui';
 	import { enhance } from '$app/forms';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	export let data;
 	export let form;
+	console.log(data);
 </script>
 
 <h1>The Admin</h1>
@@ -11,7 +13,16 @@
 	<p slot="line1">Welcome Ronald,</p>
 	<p slot="line2">What are your plans for today</p>
 </GradientHeading>
-<BrowserMockup />
+<hr />
+
+<BrowserMockup>
+	<div class="flex flex-col gap-1">
+		{#each data.demos as demo}
+			<Button class=" w-fit bg-accent-200 btn-outline" href="/">{demo.name} ({demo.niche})</Button>
+		{/each}
+	</div>
+</BrowserMockup>
+
 <VerticalTimeline />
 <input type="range" min="0" max="100" value="49" class="range" />
 <div class="overflow-x-auto"></div>

60d11e2807c652a1e0b142f93318769e52d4ea39
Ronald Otieno <ronnlats@gmail.com>
2024-08-02 11:08:51 +0300

    Update +layout.ts

--- apps/crm/src/routes/(app)/+layout.ts
+++ apps/crm/src/routes/(app)/+layout.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient, createServerClient, isBrowser, parse } from '@supabase/ssr';
 
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/private';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 import type { LayoutLoad } from '../$types';
 
